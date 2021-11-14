8fe406abae86b321b8102740d4b8e0c1610ebb1b
NextFire <git@yuru.moe>
2021-11-14 12:44:36 +0100

    fix missing icons on lxqt

--- vagrant-init.sh
+++ vagrant-init.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -x
 
+# Update arch
+pacman --noconfirm -Sy archlinux-keyring && pacman --noconfirm -Su
+
 # DE install
 pacman --noconfirm -R virtualbox-guest-utils-nox
-pacman --noconfirm -S virtualbox-guest-utils xorg-server xorg-xinit xorg-xhost lxqt noto-fonts
+pacman --noconfirm -S virtualbox-guest-utils xorg-server xorg-xinit xorg-xhost lxqt noto-fonts oxygen-icons
 systemctl enable vboxservice
 echo 'KEYMAP=fr' > /etc/vconsole.conf
 echo 'setxkbmap fr; exec startlxqt' > /home/vagrant/.xinitrc
