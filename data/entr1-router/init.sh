550528278b43ab66af116120aef0a7125e4b6940
NouhailaAchehboune <88929343+NouhailaAchehboune@users.noreply.github.com>
2022-01-15 14:11:30 +0100

    Update init.sh

--- data/entr1-router/init.sh
+++ data/entr1-router/init.sh
@@ -2,3 +2,8 @@
 set -x
 
 ip r r default via 120.0.24.2
+iptables --policy FORWARD DROP
+iptables --policy INPUT ACCEPT
+iptables --policy FORWARD ACCEPT
+iptables -A INPUT -s  192.168.255.0/255.255.255.0 -j ACCEPT
+iptables -A INPUT -s 120.0.28.0/24 -j ACCEPT 
