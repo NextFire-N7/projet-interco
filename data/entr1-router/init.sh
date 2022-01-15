99791abf9dd6a8fa2b028eae832b0e41f3e3448d
NouhailaAchehboune <88929343+NouhailaAchehboune@users.noreply.github.com>
2022-01-15 15:28:53 +0100

    Correction Nat entr1-router

--- data/entr1-router/init.sh
+++ data/entr1-router/init.sh
@@ -4,6 +4,6 @@ set -x
 ip r r default via 120.0.24.2
 iptables --policy FORWARD DROP
 iptables --policy INPUT ACCEPT
-iptables --policy FORWARD ACCEPT
+iptables --policy OUTPUT ACCEPT
 iptables -A FORWARD -s  192.168.255.0/255.255.255.0 -j ACCEPT
 iptables -A FORWARD -s 120.0.28.0/24 -j ACCEPT 
