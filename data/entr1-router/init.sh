d33d54b8dca229c00ab251ba8f164d4c1850635c
NouhailaAchehboune <88929343+NouhailaAchehboune@users.noreply.github.com>
2022-01-16 19:52:12 +0100

    communication DNS de l'AS et DNS entreprise

--- data/entr1-router/init.sh
+++ data/entr1-router/init.sh
@@ -7,3 +7,4 @@ iptables --policy INPUT ACCEPT
 iptables --policy OUTPUT ACCEPT
 iptables -A FORWARD -s  192.168.255.0/255.255.255.0 -j ACCEPT
 iptables -A FORWARD -s 120.0.28.0/24 -j ACCEPT 
+iptables -A FORWARD -s 120.0.16.10 -d 120.0.28.10 -j ACCEPT
