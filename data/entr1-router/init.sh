9370ae7f9a7d14ed3b90eb1f1ddead30e745cb71
NouhailaAchehboune <88929343+NouhailaAchehboune@users.noreply.github.com>
2022-01-17 19:10:28 +0100

    Update init.sh

--- data/entr1-router/init.sh
+++ data/entr1-router/init.sh
@@ -6,10 +6,10 @@ iptables -F
 iptables --policy FORWARD DROP
 iptables --policy INPUT DROP
 iptables --policy OUTPUT DROP
-iptables -t nat -p udp --dport 53 -j ACCEPT
+iptables -t nat -p udp --dport 53 --sport 53 -j ACCEPT
 iptables -A INPUT -m state -state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m state -state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 iptables -t nat -p udp --dport 1194 -j ACCEPT
-
+iptables -t nat -p sip -j ACCEPT
