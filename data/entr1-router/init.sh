b4cce3c86e82d32886df8b18047f887dbb3f6605
NouhailaAchehboune <88929343+NouhailaAchehboune@users.noreply.github.com>
2022-01-17 18:53:40 +0100

    update nat entreprise

--- data/entr1-router/init.sh
+++ data/entr1-router/init.sh
@@ -2,9 +2,14 @@
 set -x
 
 ip r r default via 120.0.24.2
+iptables -F
 iptables --policy FORWARD DROP
-iptables --policy INPUT ACCEPT
-iptables --policy OUTPUT ACCEPT
-iptables -A FORWARD -s  192.168.255.0/255.255.255.0 -j ACCEPT
-iptables -A FORWARD -s 120.0.28.0/24 -j ACCEPT 
-iptables -A FORWARD -s 120.0.16.10 -d 120.0.28.10 -j ACCEPT
+iptables --policy INPUT DROP
+iptables --policy OUTPUT DROP
+iptables -t nat -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -m state -state RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -m state -state RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+iptables -t nat -p udp --dport 1194 -j ACCEPT
+
