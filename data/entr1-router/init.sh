abe23eb84f9e33d65b0730824b73d6d375dbeb42
NouhailaAchehboune <nouhailaachehboune12@gmail.com>
2022-01-18 09:28:28 +0100

    init

--- data/entr1-router/init.sh
+++ data/entr1-router/init.sh
@@ -2,20 +2,59 @@
 set -x
 
 ip r r default via 120.0.24.2
-iptables -F
-iptables --policy FORWARD DROP
-iptables --policy INPUT DROP
-iptables --policy OUTPUT DROP
-iptables -t nat -p udp --dport 53 --sport 53 -j ACCEPT
-iptables -A INPUT -m state -state RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -m state -state RELATED,ESTABLISHED -j ACCEPT
-iptables -t nat -p tcp --dport 80 -j ACCEPT
-iptables -t nat -p tcp --dport 443 -j ACCEPT
-iptables -t nat -p tcp --sport 80 -j ACCEPT
-iptables -t nat -p tcp --sport 443 -j ACCEPT
-iptables -t nat -p udp --dport 1194 -j ACCEPT
-iptables -t nat -p udp --sport 1194 -j ACCEPT
-iptables -t nat -p tcp --dport 1194 -j ACCEPT
-iptables -t nat -p tcp --sport 1194 -j ACCEPT
-iptables -t nat -p sip -j ACCEPT
-iptables -A FORWARD -s 120.0.20.0/22 -j ACCEPT
+#!/bin/bash
+# Flushing all rules
+iptables -F FORWARD
+iptables -F INPUT
+iptables -F OUTPUT
+iptables -X
+# Setting default filter policy
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+# Allow unlimited traffic on loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+# Accept inbound TCP packets
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# Allow incoming SSH
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW -s 0.0.0.0/0 -j ACCEPT
+# Allow incoming OpenVPN
+iptables -A INPUT -p udp --dport 1194 -m state --state NEW -s 0.0.0.0/0 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m state --state NEW -s 0.0.0.0/0 -j ACCEPT
+# Accept outbound packets
+iptables -I OUTPUT 1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# Allow DNS outbound
+iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
+# Allow HTTP outbound
+iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+# Allow HTTPS outbound
+iptables -A OUTPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+# Enable NAT for the VPN
+iptables -t nat -A POSTROUTING -s 120.0.20.0/22 -o eth0 -j MASQUERADE
+# Allow TUN interface connections to OpenVPN server
+iptables -A INPUT -i tun0 -j ACCEPT
+# Allow TUN interface connections to be forwarded through other interfaces
+iptables -A FORWARD -i tun0 -j ACCEPT
+iptables -A OUTPUT -o tun0 -j ACCEPT
+iptables -A FORWARD -i tun0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
+# Allow outbound access to all networks on the Internet from the VPN
+iptables -A FORWARD -i tun0 -s 120.0.20.0/22 -d 0.0.0.0/0 -j ACCEPT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
